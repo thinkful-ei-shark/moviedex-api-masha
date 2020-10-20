2bd05a3268d4da57fac1fda14b37fb83cd7c7f42
Marc Trius <derpayatz@gmail.com>
2020-10-20 14:08:08 -0500

    implement Bearer authorization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
+const data = require('./movies-data-small.json');
 
 require('dotenv').config();
 
@@ -8,6 +11,32 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
 
-console.log(API_KEY);
+const requireToken = (req, res, next) => {
+  const token = req.get('Authorization') || '';
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: 'Authorization token missing' });
+  if (!token.startsWith('Bearer '))
+    return res
+      .status(401)
+      .json({ message: 'Bearer authorization required' });
+  if (token.split(' ')[1] !== API_KEY)
+    return res
+      .status(401)
+      .json({message: 'Invalid credentials'});
+  next();
+};
+
+app.use(requireToken);
+
+app.get('/movie', (req, res) => {
+  console.log('Authorized');
+  return res
+    .status(200)
+    .json(data);
+});
+
+app.listen(8080, console.log('Server on 8080'));
