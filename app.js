b5afc51f440b8aa863f84f1fe34cd166f953689d
Marc Trius <derpayatz@gmail.com>
2020-10-20 15:09:42 -0500

    switch to full size dataset

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const data = require('./movies-data-small.json');
+const data = require('./movies-data.json');
 
 require('dotenv').config();
 
